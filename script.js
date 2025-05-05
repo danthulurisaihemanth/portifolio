9c31b70a1722122f31b6b2329e360e822c188bf0
Danthuluri sai hemanth varma <119394989+danthulurisaihemanth@users.noreply.github.com>
2025-05-05 14:05:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of form validation or interactivity you can add
+document.querySelector("form").addEventListener("submit", function(event) {
+    alert("Message sent successfully!");
+});
